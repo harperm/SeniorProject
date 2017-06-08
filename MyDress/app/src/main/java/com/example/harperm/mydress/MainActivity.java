04b2c94a4bbd47569771340a4ca1d030dc487ac9
Mallory Harper <harperm@wit.edu>
2017-06-07 20:36:45 -0400

    Merge Conflict

---
+++ MyDress/app/src/main/java/com/example/harperm/mydress/MainActivity.java
@@ -0,0 +1,240 @@
+package com.example.harperm.mydress;
+
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.design.widget.BottomNavigationView;
+import android.support.v7.app.AppCompatActivity;
+import android.view.MenuItem;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.view.View;
+import java.io.File;
+import android.content.Intent;
+import android.net.Uri;
+import android.provider.MediaStore;
+import java.io.IOException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import android.os.Environment;
+import android.support.v4.content.FileProvider;
+
+import com.google.firebase.auth.FirebaseAuth; //For Firbase Login
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.auth.AuthResult;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import android.util.Log;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    private TextView mTextMessage;
+
+    //private FirebaseAuth mAuth;
+    //private FirebaseAuth.AuthStateListener mAuthListener;
+    private static final String TAG = "EmailPassword";
+
+    // [START initialize_auth]
+    private FirebaseAuth mAuth = FirebaseAuth.getInstance();
+    // [END initialize_auth]
+
+    private FirebaseAuth.AuthStateListener mAuthListener = new FirebaseAuth.AuthStateListener() {
+        @Override
+        public void onAuthStateChanged(@NonNull FirebaseAuth firebaseAuth) {
+            FirebaseUser user = firebaseAuth.getCurrentUser();
+            if (user != null) {
+                // User is signed in
+                 Log.d(TAG, "onAuthStateChanged:signed_in:" + user.getUid());
+            } else {
+                // User is signed out
+                 Log.d(TAG, "onAuthStateChanged:signed_out");
+            }
+            // ...
+        }
+    };
+
+    @Override
+    public void onStart() {
+        super.onStart();
+        // Check if user is signed in (non-null) and update UI accordingly.
+        FirebaseUser currentUser = mAuth.getCurrentUser();
+        //updateUI(currentUser);
+    }
+
+    @Override
+    public void onStop() {
+        super.onStop();
+        if (mAuthListener != null) {
+            mAuth.removeAuthStateListener(mAuthListener);
+        }
+    }
+
+    private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener
+            = new BottomNavigationView.OnNavigationItemSelectedListener() {
+
+        @Override
+        public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+            switch (item.getItemId()) {
+                case R.id.navigation_home:
+                    mTextMessage.setText(R.string.title_home);
+                    return true;
+                case R.id.navigation_dashboard:
+                    mTextMessage.setText(R.string.title_dashboard);
+                    return true;
+                case R.id.navigation_notifications:
+                    mTextMessage.setText(R.string.title_notifications);
+                    return true;
+            }
+            return false;
+        }
+
+    };
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.login_screen);
+
+        //mTextMessage = (TextView) findViewById(R.id.message);
+        //BottomNavigationView navigation = (BottomNavigationView) findViewById(R.id.navigation);
+        //navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
+    }
+
+    public void homepage (View view) {
+        setContentView(R.layout.activity_main);
+    }
+
+    public void navigateLogin (View View){
+        setContentView(R.layout.login_screen);
+    }
+
+    public void login (View view) {
+        String email = ((EditText)findViewById(R.id.emailAddress)).getText().toString();
+        String password = ((EditText)findViewById(R.id.password)).getText().toString();
+
+        mAuth.signInWithEmailAndPassword(email, password)
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()) {
+                            // Sign in success, update UI with the signed-in user's information
+                            Log.d(TAG, "signInWithEmail:success");
+                            FirebaseUser user = mAuth.getCurrentUser();
+//                            updateUI(user);
+                            Toast.makeText(MainActivity.this, "Authentication Successful.",
+                                    Toast.LENGTH_SHORT).show();
+                            setContentView(R.layout.activity_main);
+                        } else {
+                            // If sign in fails, display a message to the user.
+                            Log.w(TAG, "signInWithEmail:failure", task.getException());
+                            Toast.makeText(MainActivity.this, "Authentication failed.",
+                                    Toast.LENGTH_SHORT).show();
+//                            updateUI(null);
+                        }
+
+                        // ...
+                    }
+                });
+    }
+
+    public void openCloset(View view) {
+        setContentView(R.layout.closet_page1);
+    }
+
+    public void openLaundry(View view){
+        setContentView(R.layout.laundry_page1);
+    }
+
+    public void openShopping(View view){
+        setContentView(R.layout.shopping_page1);
+    }
+
+    public void navigateHome (View view) {
+        setContentView(R.layout.activity_main);
+    }
+
+    public void aeRedirect (View view){
+        Intent viewIntent = new Intent("android.intent.action.VIEW", Uri.parse("http://www.ae.com/"));
+            startActivity(viewIntent);
+    }
+
+    public void gapRedirect (View view){
+        Intent viewIntent = new Intent("android.intent.action.VIEW", Uri.parse("http://www.gap.com/"));
+        startActivity(viewIntent);
+    }
+
+    public void lordRedirect (View view){
+        Intent viewIntent = new Intent("android.intent.action.VIEW", Uri.parse("https://www.lordandtaylor.com/"));
+        startActivity(viewIntent);
+    }
+
+    public void nordRedirect (View view){
+        Intent viewIntent = new Intent("android.intent.action.VIEW", Uri.parse("http://shop.nordstrom.com/"));
+        startActivity(viewIntent);
+    }
+
+    public void tjRedirect (View view){
+        Intent viewIntent = new Intent("android.intent.action.VIEW", Uri.parse("http://www.tjmaxx.com/"));
+        startActivity(viewIntent);
+    }
+
+    public void jcrewRedirect (View view){
+        Intent viewIntent = new Intent("android.intent.action.VIEW", Uri.parse("http://www.jcrew.com/"));
+        startActivity(viewIntent);
+    }
+    static final int REQUEST_IMAGE_CAPTURE = 1;  //We only need it to take one image capture
+    public void openCamera (View view){
+        Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        if (takePictureIntent.resolveActivity(getPackageManager()) != null) {
+            startActivityForResult(takePictureIntent, REQUEST_IMAGE_CAPTURE);
+        }
+        //dispatchTakePictureIntent();
+        //galleryAddPic();
+    }
+
+    public void createAccount (View view){
+        setContentView(R.layout.create_account);
+    }
+    public void registerUser(View view) { //(String email, String password) {
+        String email = ((EditText)findViewById(R.id.emailAddress)).getText().toString();
+        String password = ((EditText)findViewById(R.id.password)).getText().toString();
+        String passwordConfirm = ((EditText)findViewById(R.id.passwordConfirm)).getText().toString();
+
+        mAuth.createUserWithEmailAndPassword(email, password)
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        Log.d(TAG, "createUserWithEmail:onComplete:" + task.isSuccessful());
+
+                        // If sign in fails, display a message to the user. If sign in succeeds
+                        // the auth state listener will be notified and logic to handle the
+                        // signed in user can be handled in the listener.
+                        if (task.isSuccessful()) {
+                            // Sign in success, update UI with the signed-in user's information
+                            Log.d(TAG, "createUserWithEmail:success");
+                            FirebaseUser user = mAuth.getCurrentUser();
+//                            updateUI(user);
+                            Toast.makeText(MainActivity.this, "Authentication Success.",
+                                  Toast.LENGTH_SHORT).show();
+                        } else {
+                            // If sign in fails, display a message to the user.
+                            Log.w(TAG, "createUserWithEmail:failure", task.getException());
+                            Toast.makeText(MainActivity.this, "Authentication failed.",
+                                    Toast.LENGTH_SHORT).show();
+//                            updateUI(null);
+                        }
+
+                        // ...
+                    }
+                });
+    }
+    public void forgotPassword (View view){
+        setContentView(R.layout.forgot_password);
+    }
+
+
+
+
+
+
+}
